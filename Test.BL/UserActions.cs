de40e5c5a8c5c998e34b1aa0e55992e49914ed03
Abhijeet Trivedi <abhijeet.trivedi@gatewaygroup.com>
2020-03-17 19:09:47 +0530

    first commit

---
+++ Test.BL/UserActions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.EL;
+using Test.EL.Models;
+
+namespace Test.BL
+{
+    public class UserActions 
+    {
+        public Object  UserList()
+        {
+            using (var db = new AbhijeetDbEntities())
+            {
+                var data = db.Users.ToList();
+                return data;
+            }
+            
+        }
+
+        public void AddUser(User obj)
+        {
+            using (var db = new AbhijeetDbEntities())
+            { 
+                db.Users.Add(obj);
+                db.SaveChanges();
+            }
+        }
+
+        
+    }
+}
---
+++ TestAssessment/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.EL.Models;
+using Test.BL;
+
+
+namespace TestAssessment.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var userBL = new UserActions();
+            var userData = userBL.UserList();
+            return View(userData);
+                
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User obj)
+        {
+            var userBL = new UserActions();
+            userBL.AddUser(obj);
+            return RedirectToAction("Index");
+
+
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+    }
+}
